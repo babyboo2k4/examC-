0d2823d96bf947b0315ad2b7920e2db3f3393d76
babyboo2k4 <144683950+babyboo2k4@users.noreply.github.com>
2024-07-24 19:46:47 +0700

    Add files via upload

---
+++ FindWordInString.cs
@@ -0,0 +1,33 @@
+﻿namespace ApplicationProgramming;
+
+public class FindWordInString
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter string:");
+        string inputString = Console.ReadLine();
+        Console.WriteLine("Enter word want to search:");
+        string wordtoSearch = Console.ReadLine();
+        int count = countOccurrences(inputString, wordtoSearch);
+        Console.WriteLine($"Word found {count} times in the string");
+
+        int countOccurrences(string str,
+            string word)
+        {
+            
+            string[] a = str.Split(' ');
+    
+    
+            count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (word.Equals(a[i]))
+                    count++;
+            }
+    
+            return count;
+        }
+        {
+        }
+    }
+}
---
+++ People.cs
@@ -0,0 +1,21 @@
+﻿// namespace ApplicationProgramming;
+//
+// public class People
+// {
+//     public string Name { get; set; }
+//     public string Gender { get; set; }
+//     public int Age { get; set; }
+//
+//     public static void Main(string[] args)
+//     {
+//         People people = new People
+//         {
+//             Name = "Eric Cantona",
+//             Gender = "Male",
+//             Age = 58
+//         };
+//         Console.WriteLine($"Person details: Name ={people.Name} , Gender={people.Gender}, Age = {people.Age}" );
+//         people.Age += 10;
+//         Console.WriteLine($"Person details(after +10): Name={people.Name}, Gender={people.Gender}, Age ={people.Age}");
+//     }
+// }
